2a04c8e8a0574b5bc44d407758c7cd616729d4f1
Camilo Ordonez <icoj95@gmail.com>
2015-06-02 12:52:30 -0700

    Adding CRU, missin D

---
+++ server.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var app = express();
+//Set up PG and models sequelize
+var pg = require("pg"); //It requires the postgresmodel
+var models = require("./models/index.js"); 
+// var request = require("request");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+// Links to public folder
+app.use(express.static(__dirname + '/public'));
+
+app.set("view engine", "ejs");
+
+//Set up method override --> It helps the form to set the put and delete 
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+
+app.get('/', function(req,res){
+   models.Chirp.findAll().done(function(messages, error){
+      res.render("index", {
+         allMessages: messages
+      });
+   });
+});
+
+
+app.get('/edit/:id', function(req,res){
+   models.Chirp.findById(req.params.id).done(function(message){
+      res.render("edit", {
+         chirpMessage: message
+      });   
+   });
+   
+});
+
+app.post('/add', function(req,res){
+   models.Chirp.create({
+      message: req.body.message
+   }).done(function(){
+      res.redirect("/");
+   });
+});
+
+app.put('/edit/:id', function(req,res){
+   models.Chirp.findById(req.params.id).done(function(message){
+      console.log("getting here");
+      message.updateAttributes({
+         message:req.body.message
+      }).done(function(){
+         res.redirect('/');
+      });
+   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
