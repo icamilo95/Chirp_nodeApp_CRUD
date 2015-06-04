8b8c7f76448810cdd42c4ab5428709c522955c49
Camilo Ordonez <icoj95@gmail.com>
2015-06-04 01:29:39 -0700

    changed database name in migration to chirps line 4 and 28

--- migrations/20150531145037-create-chirp.js
+++ migrations/20150531145037-create-chirp.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Chirps', {
+    return queryInterface.createTable('chirps', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Chirps');
+    return queryInterface.dropTable('chirps');
   }
 };
