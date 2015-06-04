0ef11167b8d762c0dff693d6a9d74eb4d78f4d7a
Camilo Ordonez <icoj95@gmail.com>
2015-06-04 01:19:34 -0700

    working on tests to deploy to heroku

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 var pg = require("pg"); //It requires the postgresmodel
 var models = require("./models/index.js"); 
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM "Chirps"');
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM Chirps');
 
-  query.on('row', function(row) {
-    console.log("Cami", JSON.stringify(row));
-  });
-});
+//   query.on('row', function(row) {
+//     console.log("Cami", JSON.stringify(row));
+//   });
+// });
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
